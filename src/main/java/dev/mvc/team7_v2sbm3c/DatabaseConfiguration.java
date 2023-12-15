dae5fd58abaa4171eaa777e0cb83f126ef074010
LeeDongWoo99 <dleh684@gmail.com>
2023-12-15 12:42:45 +0900

    12/15 DatabaseConfiguration 'dev.mvc.qna'추가

--- src/main/java/dev/mvc/team7_v2sbm3c/DatabaseConfiguration.java
+++ src/main/java/dev/mvc/team7_v2sbm3c/DatabaseConfiguration.java
@@ -20,11 +20,8 @@ import com.zaxxer.hikari.HikariDataSource;
 @PropertySource("classpath:/application.properties")  // 설정 파일 위치
 
 @MapperScan(basePackages= {"dev.mvc.model","dev.mvc.master","dev.mvc.car","dev.mvc.customer", "dev.mvc.condition", "dev.mvc.recommend",
-<<<<<<< HEAD
-                                          "dev.mvc.good", "dev.mvc.chatbot", "dev.mvc.qna" })
-=======
-                                          "dev.mvc.good", "dev.mvc.chatbot", "dev.mvc.sms" })
->>>>>>> d460292e1c5b596e62d025ec845251cb5af248aa
+                                          "dev.mvc.good", "dev.mvc.chatbot", "dev.mvc.sms" ,"dev.mvc.qna" })
+
 public class DatabaseConfiguration { 
 
     
